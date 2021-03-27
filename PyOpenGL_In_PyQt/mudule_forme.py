ed57ffc5b67c37e7352aaea48c55d15d4b70bb30
TO <The0.walt3r@gmail.com>
2021-03-27 18:56:41 +0100

    Update mudule_forme.py

--- PyOpenGL_In_PyQt/mudule_forme.py
+++ PyOpenGL_In_PyQt/mudule_forme.py
@@ -1,87 +1,33 @@
 from copy import deepcopy
+from Vertex import Vertex
 
 
-class Vertex:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.vertex = [x, y, z]
-
-    def __add__(self, other):
-        if type(other) == Vertex:
-            coeff = [None, None, None]
-            for i in range(len(self.vertex)):
-                coeff[i] = self.vertex[i] + other.vertex[i]
-            return Vertex(*coeff)
-        if type(other) == str:
-            if other == "x":
-                self.vertex[0] += 1
-
-            elif other == "y":
-                self.vertex[1] += 1
-            elif other == "z":
-                self.vertex[2] += 1
-
-            return Vertex(*self.vertex)
-
-    def __sub__(self,other):
-        if type(other) == Vertex:
-            coeff = [None, None, None]
-            for i in range(len(self.vertex)):
-                coeff[i] = self.vertex[i] - other.vertex[i]
-            return Vertex(*coeff)
-        if type(other) == str:
-            if other == "x":
-                self.vertex[0] -= 1
-
-            elif other == "y":
-                self.vertex[1] -= 1
-            elif other == "z":
-                self.vertex[2] -= 1
-
-            return Vertex(*self.vertex)
-
-
-    def __str__(self):
-        return f"{self.vertex}"
-
-    def __getitem__(self, item):
-        if type(item) == int:
-            return self.vertex[item]
-
-    def __mul__(self, other):
-        if type(other) == int:
-            for i in range(len(self.vertex)):
-                self.vertex[i] *= other
-        elif type(other) == Vertex:
-            for i in range(len(self.vertex)):
-                pass
-
-
-class Cube:
-    def __init__(self, origine=[0, 0, 0]):
+class Cube:  # renvoi les vertices et les edges
+    def __init__(self, longueur=1, origine=[0, 0, 0]):
         self.origine = Vertex(*origine)
+        self.longueur = longueur
         self.vertices = self.gen()[0]
         self.edges = self.gen()[1]
 
-    def gen(self, longueur=1):
+
+    def gen(self):
         vertices = [
              self.origine.vertex,
-             (deepcopy(self.origine) + "x").vertex,
-             (deepcopy(self.origine) + "y").vertex,
-             (deepcopy(self.origine) + "z").vertex,
-             (deepcopy(self.origine) + "x" + "y").vertex,
-             (deepcopy(self.origine) + "x" + "y" + "z").vertex,
-             (deepcopy(self.origine) + "x" + "z").vertex,
-             (deepcopy(self.origine) + "z" + "y").vertex
+             (deepcopy(self.origine) + ("x" + str(self.longueur))).vertex,
+             (deepcopy(self.origine) + ("y" + str(self.longueur))).vertex,
+             (deepcopy(self.origine) + ("z" + str(self.longueur))).vertex,
+             (deepcopy(self.origine) + ("x" + str(self.longueur)) + ("y" + str(self.longueur))).vertex,
+             (deepcopy(self.origine) + ("x" + str(self.longueur)) + ("y" + str(self.longueur)) + ("z" + str(self.longueur))).vertex,
+             (deepcopy(self.origine) + ("x" + str(self.longueur)) + ("z" + str(self.longueur))).vertex,
+             (deepcopy(self.origine) + ("z" + str(self.longueur)) + ("y" + str(self.longueur))).vertex
             ]
         edges = ((0, 1), (0, 2), (0, 3), (1, 4), (1, 6), (2, 7),
                  (4, 5), (5, 6), (3, 6), (5, 7), (2, 4), (3, 7))
         return vertices, edges
 
+
 class Grid:
-    def __init__(self, rayon,origine=[0, 0, 0]):
+    def __init__(self, rayon, origine=[0, 0, 0]):
         self.origine = Vertex(*origine)
         self.rayon = rayon
         self.vertices = self.gen()[0]
@@ -111,12 +57,12 @@ class Grid:
         for i in range((2 * self.rayon + 1) * (2 * self.rayon + 1)):
             if (i+1)%(2*self.rayon+1)==0:
                 pass
-            else :
+            else:
                 edges.append((i,i+1))
 
         return vertices, edges
-    
-    class Grille_opti:
+
+class Grille_opti:
     def __init__(self, rayon, centre=[0,0,0]):
         self.origine = Vertex(*centre)
         self.rayon = rayon
@@ -143,3 +89,4 @@ class Grid:
                 edges.append((i, i+1))
 
         return vertices, edges
+
